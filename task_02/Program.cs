014ebad02413612a7f7fe11eccdbfe24758683cd
kristina <arlauskaitekristina@mail.ru>
2022-06-27 17:03:37 +0300

    изменен task_03

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -2,7 +2,8 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.WriteLine("Эта программа выдает название дня недели по заданному номеру.Введите цифру от 1 до 7: ");
+Console.Write("Эта программа выдает название дня недели по заданному номеру.\nВведите цифру от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
 if (day == 1) Console.WriteLine("Понедельник");
 else if (day == 2) Console.WriteLine("Вторник");
 else if (day == 3) Console.WriteLine("Среда");
