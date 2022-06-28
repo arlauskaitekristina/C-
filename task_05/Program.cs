e08a454791f4cb0f2ca4e5539c173ff0cd86a2fd
kristina <arlauskaitekristina@mail.ru>
2022-06-28 21:43:19 +0300

    d task_05

--- task_05/Program.cs
+++ task_05/Program.cs
@@ -2,9 +2,17 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> "-2, -1, 0, 1, 2"
 
+Console.WriteLine("Эта программа на вход принимает одно число (N), а на выходе показыает все целые числа в промежутке от -N до N");
+Console.Write("Введите число: ");
+
 int num = Convert.ToInt32(Console.ReadLine());
-for (int index = -num; index <= num; index++);
+int i = Convert.ToInt32(Console.ReadLine());
+if (int i = -num; i <= num; i ++);
 {
-    Console.Write($"{index} ");
+    Console.Write($"{i} ");
+}
+else
+{
+    Console.WriteLine("Это не число");
 }
 
