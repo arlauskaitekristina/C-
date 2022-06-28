6b0d249c7e76658db3102e35845b271f257eaf01
kristina <arlauskaitekristina@mail.ru>
2022-06-28 21:54:08 +0300

    Изменен task_08

--- task_08/Program.cs
+++ task_08/Program.cs
@@ -3,6 +3,7 @@
 // 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Эта программа принимает на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N");
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
