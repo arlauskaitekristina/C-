877ae1a2418a11bc96811c802a1d086956ee2f50
kristina <arlauskaitekristina@mail.ru>
2022-06-28 21:52:43 +0300

    Изменен task_03

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -3,8 +3,12 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Эта программа находит наибольшее из двух чисел\nВведите первое число: ");
+Console.Write("Эта программа находит наибольшее из двух чисел");
+
+Console.Write("Введите первое число: ");
 int num_1 = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите второе число: ");
 int num_2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Наибольшее число = " + (num_1 > num_2 ? num_1 : num_2));
