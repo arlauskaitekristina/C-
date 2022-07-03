325154a34cb845c7b19d794b0f2e790c65c96fae
kristina <arlauskaitekristina@mail.ru>
2022-07-03 23:18:32 +0300

    add task_11

--- task_11/Program.cs
+++ task_11/Program.cs
@@ -3,13 +3,28 @@
 // 782 -> 72
 // 918 -> 98
 
+// Console.WriteLine("Эта программа выводит случайное трехзначное число и удаляет вторую цифру этого числа");
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int firstDigit = (num / 100) * 10;
+// int thirdDigit = (num % 10);
+
+// int result = firstDigit + thirdDigit;
+// Console.WriteLine(result);
+
 Console.WriteLine("Эта программа выводит случайное трехзначное число и удаляет вторую цифру этого числа");
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
+var rndNum = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число: {rndNum}");
 
-int firstDigit = (num / 100) * 10;
-int thirdDigit = (num % 10);
+int CutMid (int num)
+{
+int firstDig = num / 100 * 10;
+int secondDig = num % 10;
+return firstDig + secondDig;
+}
+int result = CutMid(rndNum);
 
-int result = firstDigit + thirdDigit;
-Console.WriteLine(result);
+Console.WriteLine($"Полученное двухзначное число(через метод): {result}");
