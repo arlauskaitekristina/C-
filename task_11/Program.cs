1a7a7df5cc328ff3c7071ef7449fe9176bce0e12
kristina <arlauskaitekristina@mail.ru>
2022-07-03 15:56:09 +0300

    add task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Наишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.WriteLine("Эта программа выводит случайное трехзначное число и удаляет вторую цифру этого числа");
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int firstDigit = (num / 100) * 10;
+int thirdDigit = (num % 10);
+
+int result = firstDigit + thirdDigit;
+Console.WriteLine(result);
