c757eaa735160854415659dc3623aee49a079d10
kristina <arlauskaitekristina@mail.ru>
2022-07-11 00:28:25 +0300

    add task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, заполненный псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Эта программа задаёт массив из 8 элементов, заполненный псевдослучайными числами и выводит их на экран.");
+
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+int[] resArray = CreateArrayRndInt(8);
+PrintArray(resArray);
