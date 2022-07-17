eecdc4c77be3e56846ef5b3bd9bc288b054630b8
kristina <arlauskaitekristina@mail.ru>
2022-07-17 12:27:42 +0300

    изменен task_36

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -1,32 +1,50 @@
-﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.\
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
 Console.WriteLine("Эта программа задаёт одномерный массив, заполненный случайными числами, и находит сумму элементов, стоящих на нечётных позициях.");
+Console.Write("Введите размер массива: ");
+int intsize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное число диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Введи количество элементов массива: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int CreateNumRnd(int num, int min, int max)
+int[] CreateArrayRndInt(int size, int begin, int end)
 {
-    int[] CreateNumRnd = new int [num];
-    int SumElem = 0;
-    Console.Write($"Получившийся массив: ");
-
-    for (int i = 0; i < CreateNumRnd.Length; i++)
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
     {
-        CreateNumRnd[i] = new Random().Next(min, max);
-
-        Console.Write(CreateNumRnd[i] + " ");
-
-        if (i % 2 != 1)
-        {
-            SumElem = SumElem + CreateNumRnd[i];
-        }
+        array[i] = rnd.Next(begin, end + 1);
     }
-    return SumElem;
+    return array;
 }
 
-int createNumRnd = CreateNumRnd (num, 1, 10);
+void PrintArr(int[] array)
+{
+    Console.WriteLine($"Сгенерированный массив из {array.Length} элементов: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    
+    }
+}
 
-Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {createNumRnd}");
+int SumNum(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+int[] array = CreateArrayRndInt(intsize, min, max); 
+PrintArr(array);
+int result = SumNum(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {result}");
+
