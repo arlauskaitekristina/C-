b05277fcbc7f535476a6498cad86c1d337212266
kristina <arlauskaitekristina@mail.ru>
2022-07-17 18:33:39 +0300

    add task_40

---
+++ task_40/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход тричисла и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+int a;
+int b;
+int c;
+bool able;
+string answer;
+
+Console.WriteLine("Эта программа принимает на вход тричисла и проверяет, может ли существовать треугольник с сторонами такой длины");
+Console.WriteLine("Введите сторону треугольника А: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника В: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника С: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+able = CheckAbilityTracl(a, b, c);
+answer = ShowAnswer(able);
+Console.WriteLine(answer);
+
+bool CheckAbilityTracl(int a, int b, int c)
+{
+    if (a < (b + c) && b < (a + c) && c < (a + b))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+string ShowAnswer(bool ability)
+{
+    if (ability)
+    {
+        return "Треугольник возможен";
+    }
+    else
+    {
+        return "Треугольник не возможен";
+    }
+}
