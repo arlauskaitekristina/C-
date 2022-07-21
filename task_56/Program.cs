61bca43b4ff103b94de680e8b605750f6b8b9319
kristina <arlauskaitekristina@mail.ru>
2022-07-21 18:00:05 +0300

    add task_56

---
+++ task_56/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.WriteLine("Эта программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов.");
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число дипазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    Random rnd = new Random();
+
+    var matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int row = matrix.GetLength(0);
+    int col = matrix.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MinElemSum(int[,] matrix)
+{
+    int row = matrix.GetLength(0);
+    int col = matrix.GetLength(1);
+    int minimumSum = Int32.MaxValue;
+    int result = 0;
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (sum < minimumSum)
+        {
+            minimumSum = sum;
+            result = i+1;
+        }
+    }
+    return result;
+}
+
+
+int[,] matrix = CreateMatrixRndInt(row, col, min, max);
+Console.WriteLine($"Сгенерированная матрица {row}x{col}:");
+PrintMatrix(matrix);
+Console.WriteLine("Строка с наименьшей суммой элементов: " + MinElemSum(matrix));
+
