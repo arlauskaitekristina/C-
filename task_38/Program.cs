1081556e1b0793c6f44aa4588d7badab00b45c1f
kristina <arlauskaitekristina@mail.ru>
2022-07-30 17:43:06 +0300

    измене task_38

--- task_38/Program.cs
+++ task_38/Program.cs
@@ -4,24 +4,24 @@
 Console.WriteLine("Эта программа задаёт массив вещественных чисел и находит разницу между максимальным и минимальным элементов массива.");
 
 Console.WriteLine("Введите размер массива: ");
-double intsize = Convert.ToDouble(Console.ReadLine());
+int intsize = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите минимальное число диапазона: ");
-double min = Convert.ToDouble(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное число: ");
-double max = Convert.ToDouble(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 
-double[] CreateArr()
+double[] CreateArr(int size, int min, int max)
 {
-    array = new double[size];
     Random rnd = new Random();
+    var array = new double[size];
     for (int i = 0; i < array.Length; i++)
     {
-        array [i] = rnd.Next();
+        array[i] = rnd.NextDouble() * (max - min) + min;
     }
     return array;
 }
 
-void PrintArr(double[] array, int Round = 2)
+void PrintArr(double[] array, int round = 2)
 {
     Console.WriteLine($"Сгенерированный массив из {array.Length} элементов: ");
     for (int i = 0; i < array.Length; i++)
@@ -29,23 +29,22 @@ void PrintArr(double[] array, int Round = 2)
         if (i == 0) Console.Write("[");
         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.WriteLine($"{array[i]}]");
-
     }
 }
 
-double SearchElem (double[]array) 
+double SearchElem(double[] array)
 {
-double max=array[0];
-double min=array[0];
+    double max = array[0];
+    double min = array[0];
     for (int i = 1; i < array.Length; i++)
     {
         if (max < array[i]) max = array[i];
         if (min > array[i]) min = array[i];
     }
 
-    return  max - min;
+    return max - min;
 }
-double[] arr = CreateArr(5, );
+double[] arr = CreateArr(intsize, m, n);
 PrintArr(arr);
-double result = SearchElem (arr);
+double result = SearchElem(arr);
 Console.WriteLine($"\nРазница между между максимальным и минимальным элементами: {result}");
