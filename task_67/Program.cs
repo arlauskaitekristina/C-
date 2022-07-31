64e7575bfd3f6b18536323a1de11477cf5fed8b1
kristina <arlauskaitekristina@mail.ru>
2022-07-31 15:13:38 +0300

    add task_67

---
+++ task_67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+int sum = NaturalNumber(n, 0);
+Console.WriteLine(sum);
+
+int NaturalNumber(int n, int sum)
+{
+    sum = sum + n % 10;
+    if (n / 10 == 0) return sum;
+    else
+    {
+        return NaturalNumber (n / 10, sum);
+    }
+}
